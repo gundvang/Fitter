5e4a2eaa69fe18c80dcff30c347835ffe24e0f51
Michael Gundvang <kontakt@gundvang.dk>
2015-08-18 19:32:06 +0200

    updated

--- script.js
+++
@@ -1,8 +0,0 @@
-
-	function contentFit() {
-		var rx = 1.95;
-		var ww = $(window).width();
-		$('.fitter').css({ 'height':ww/rx, 'width':'100%' });
- 		$('.fitter').css({ 'margin-top': '-'+(ww/rx)/2+'px'});
-		$('*[data-fontsize]').each(function(){ $(this).css({'font-size': parseFloat($(this).data('fontsize') * $(window).width() / 100) + 'px', 'line-height': parseFloat($(this).data('fontsize') * $(window).width() / 80) + 'px' }) });
-	}
