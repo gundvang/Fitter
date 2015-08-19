cba84381c71ceceb6ae129bc3a1dfd1210af24dd
Michael Gundvang <kontakt@gundvang.dk>
2015-08-19 22:33:39 +0200

    updated readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# Fitter (in development)
+Fitter makes it possible to wrap elements in wrappers that maintain a set ratio even when resized and center it in the viewport if needed.
+
+## Requirements
+Fitter requires [jQuery](https://jquery.com/) (2.1.4 minimum) for its functionality to work correctly.  
+
+
+### data-tag
+
+##### `data-fitter=""`
+
+
+## Setup customization
+```
+Fitter.setup();
+```
+
+##### `defaultRatio: 1.95`
+
+##### `defaultPosition: 'absolute'`
+
+##### `defaultOrientation: horizontal`
+
+##### `defaultLocation: center`
+
+##### `defaultSize: cover`
+
+
+## Initialization
+```
+Fitter.init();
+```
--- fitter.js
+++ fitter.js
@@ -1,67 +1,72 @@
 var Fitter = {
 	info: {
-		version: '0.1',
+		version: '0.4',
 		documentation: 'https://github.com/gundvang/fitter',
 	},
+	enabled: true,
 	settings: {
-		defaultRatio: 1.95,
+		defaultRatio: 1.95, // IDEA: screen formats 16:9, 16:10, 4:3, ?
 		defaultPosition: 'absolute',
-		defaultOrientation: 'horizontal',
+		defaultOrientation: 'vertical',
 		defaultLocation: 'center',
+		defaultSize: 'cover',
 	},
 };
 
 Fitter.change = function() {
 	$('[data-fitter]').each(function(data) {
-		var css = {},
-			orientation,
-			position;
+		var fitterData = $(this).data('fitter').split(';'),
+			fitter = {},
+			css = {};
 
-		if ($(this).data('orientation')) {
-			orientation = $(this).data('orientation');
-		} else {
-			orientation = Fitter.settings.defaultOrientation;
-		}
-		if ($(this).data('ratio')) {
-			if (orientation == 'vertical') {
-				css['width'] = $(window).height() / $(this).data('ratio');
-				css['height'] = '100%';
-			} else {
-				css['width'] = '100%';
-				css['height'] = $(window).width() / $(this).data('ratio');
-			}
-		} else {
-			if (orientation == 'vertical') {
-				css['width'] = $(window).height() / Fitter.settings.defaultRatio;
-				css['height'] = '100%';
-			} else {
-				css['width'] = '100%';
-				css['height'] = $(window).width() / Fitter.settings.defaultRatio;
-			}
+		for (i = 0; i < fitterData.length; i++) {
+			var property = fitterData[i].split(':')[0];
+			var value = fitterData[i].split(':')[1];
+			if (!property || !value) {	continue; }
+			property = property.trim();
+			value = value.trim();
+			fitter[property] = value;
 		}
 
-		if ($(this).data('position')) {
-			position = $(this).data('position');
+		if (typeof(fitter['orientation']) == 'undefined') { fitter['location'] = Fitter.settings.defaultOrientation; }
+		if (typeof(fitter['position']) == 'undefined') { fitter['position'] = Fitter.settings.defaultPosition; }
+		if (typeof(fitter['ratio']) == 'undefined') { fitter['ratio'] = Fitter.settings.defaultRatio; }
+		if (typeof(fitter['location']) == 'undefined') { fitter['location'] = Fitter.settings.defaultLocation; }
+		if (typeof(fitter['size']) == 'undefined') { fitter['size'] = Fitter.settings.defaultSize; }
+
+		css['position'] = fitter['position'];
+
+		if (fitter['orientation'] == 'vertical') {
+			css['height'] = '100%';
+			css['width'] = $(window).height() / fitter['ratio']; // FIXME: should change according to position type
 		} else {
-			position = Fitter.settings.defaultPosition;
+			css['width'] = '100%';
+			css['height'] = $(window).width() / fitter['ratio']; // FIXME: should change according to position type
 		}
-		css['position'] = position;
-		if (position == 'absolute' || position == 'fixed') {
-			if (orientation == 'vertical') {
+		if (fitter['position'] == 'absolute' || fitter['position'] == 'fixed') {
+			if (fitter['orientation'] == 'vertical') {
 				css['top'] = '0';
-				css['left'] = '50%';
-				css['transform'] = 'translateX(-50%)';
+				if (fitter['location'] == 'left') {
+					css['left'] = '0';
+				} else if (fitter['location'] == 'right') {
+					css['right'] = '0';
+				} else {
+					css['left'] = '50%';
+					css['transform'] = 'translateX(-50%)';
+				}
 			} else {
 				css['left'] = '0';
-				css['top'] = '50%';
-				css['transform'] = 'translateY(-50%)';
+				if (fitter['location'] == 'top') {
+					css['top'] = '0';
+				} else if (fitter['location'] == 'bottom') {
+					css['bottom'] = '0';
+				} else {
+					css['top'] = '50%';
+					css['transform'] = 'translateY(-50%)';
+				}
 			}
 		}
-
 		$(this).css( css );
-
-		console.log( $(this).data('fitter') );
-		console.log( css );
 	});
 };
 
@@ -70,6 +75,7 @@ Fitter.setup = function(data) {
 	if (typeof(data.position) != 'undefined') { Fitter.settings.defaultPosition = data.position; }
 	if (typeof(data.ratio) != 'undefined') { Fitter.settings.defaultRatio = data.ratio; }
 	if (typeof(data.location) != 'undefined') { Fitter.settings.defaultLocation = data.location; }
+	if (typeof(data.size) != 'undefined') { Fitter.settings.defaulSize = data.size; }
 };
 
 Fitter.init = function() {
